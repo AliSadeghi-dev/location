7f323cc1ac194cecafbc9516dea0bb981891cf22
alisadeghi <alisadghi228@gmail.com>
2022-06-15 20:01:55 +0430

    add address

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import IpInfo from "./components/IpInfo";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
+
+// 1   https://www.ipify.org/  for ip
+//2   https://ipwhois.io/documentation
+//3 https://www.npmjs.com/package/react-loader-spinner
+
 function App() {
-    return (
-      <IpInfo/>
+    return ( <
+        IpInfo / >
     );
 }
 
